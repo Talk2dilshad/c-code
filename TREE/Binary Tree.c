b5bb72162ab08423ec60dae0c207a9fb5e5d5fd4
Talk2dilshad <talkdilshad@gmail.com>
2021-03-10 16:30:05 +0530

    BINARY TREE

---
+++ TREE/Binary Tree.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    struct node *lchild;
+    struct node *rchild;
+    int data;
+}*root=NULL;
+
+void insert(int key)
+{
+    struct node *t=root;
+    // * r is tail pointer & *p create new node
+    struct node *r,*p;
+
+    if(root==NULL)
+    {
+        p=(struct node *)malloc(sizeof(struct node));
+        p->data=key;
+        p->lchild=p->rchild=NULL;
+        root=p; //copy p data into root
+        return;
+    }
+    while(t !=NULL)
+    {
+        r=t; //tailing pointer to a node
+        if(key< t->data)
+            t=t->lchild;
+        else if(key > t->data)
+            t=t->rchild;
+        else
+            return;         
+    }//after reaching point where should tree new node will be add the tailing node will help to add new node to this tree
+        p=(struct node *)malloc(sizeof(struct node));
+        p->data=key;
+        p->lchild=p->rchild=NULL;
+
+    if(key< r->data )
+        r->lchild=p;
+    else
+    r->rchild=p;    
+}
+void inorder(struct node *p)
+{
+    if(p)
+    {
+        inorder(p->lchild);
+        printf("%d ",p->data);
+        inorder(p->rchild);
+    }
+}
+int main()
+{
+    insert(10);
+    insert(5);
+    insert(20);
+    insert(8);
+    insert(30);
+
+    inorder(root);
+    printf("\n");
+    return 0;
+}
