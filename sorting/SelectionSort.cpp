e36a3afd699ed9a937d835a9b8d01d645416ab49
Talk2dilshad <talkdilshad@gmail.com>
2021-03-29 20:14:17 +0530

    selection sort completed

---
+++ sorting/SelectionSort.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+void SelectionSort(int arr[],int size)
+{
+    int j,k;
+    for(int i= 0; i<size-1; i++)
+    {
+        for(j=k=i; j<size; j++)
+        {
+            if(arr[j]<arr[k])
+            {
+                k=j;
+            }
+        }
+        swap(arr[i],arr[k]);
+    }
+}
+
+int main()
+{
+    int arr[]={11,13,7,12,16,9,24,5,10,3},size=sizeof(arr)/sizeof(arr[0]);
+    SelectionSort(arr,size);
+    //display func()
+    for (int n : arr)
+    {
+        cout << n << " ";
+    }
+}
