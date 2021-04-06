8afeacf4683eb6a7b04723c4d6fb4aba01da8dc4
Talk2dilshad <talkdilshad@gmail.com>
2021-04-06 16:08:49 +0530

    search feature added

--- Hashing/linearProbing.c
+++ Hashing/linearProbing.c
@@ -13,12 +13,22 @@ int probe(int h[],int key)
     i++;
     return (index+i)%size;
 }
+
 void Insert(int h[],int key)
 {
     int index=hash(key);
     if(h[index] !=0)//check if there is free space
     index=probe(h,key);
     h[index]=key;
+
+}
+int search(int h[],int key)
+{
+    int index=hash(key);
+    int i=0;
+    while(h[(index+i)%size] !=key)
+    i++;
+    return h[index+i]%size;
 
 }
 int main()
@@ -29,5 +39,6 @@ int main()
  Insert(ht,25);
  Insert(ht,35);
  Insert(ht,26);
+ printf("Key found at index ==> %d",search(ht,12));
  return 0;
 }
