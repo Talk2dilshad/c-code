187b6b56806df756db5dc4f1b8686257b490d50e
talkdilshad@gmail.com <talkdilshad@gmail.com>
2021-07-31 13:28:33 +0530

    stl library

---
+++ C++ STL LIBRARY/sort.cpp
@@ -0,0 +1,37 @@
+/* 
+
+Sorting is one of the most basic functions applied to data.
+It means arranging the data in a particular fashion , which can be increasing or decreasing
+There is built-in function in c++ stl by the name sort().
+This function use intro sort.
+
+The prototype for sort is : 
+
+sort(startaddress, endaddress)
+
+startaddress: the address of the first element of the array
+endaddress: the address of the next contiguous location of the last element of the array.
+
+*/
+#include <iostream>
+#include <algorithm>
+using namespace std;
+void show(int arr[],int size)
+{
+
+	cout<<endl;
+	for(int i=0; i< size; i++)
+	cout<<arr[i]<< "  ";
+	cout<<endl;
+}
+int main()
+{
+	int arr[]={1,2,3,4,5,9,8,7,6};
+	int size= sizeof(arr)/sizeof(arr[0]);
+	show(arr,size);
+	sort(arr,arr+size);
+	show(arr,size);
+
+
+}
+
